3d49e2cb0027b0ce904b8edfbc0ebb4bf27b81bb
Leng Sokchhay <mastersokchhayfap@gmail.com>
2024-06-17 13:57:59 +0700

    Update to maincontroller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,7 +7,7 @@ use App\Models\Order;
 use App\Models\OrderDetail;
 use Illuminate\Http\Request;
 
-class OrderController extends Controller
+class OrderController extends MainController
 {
     public function index()
     {
